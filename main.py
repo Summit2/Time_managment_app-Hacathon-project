e5062e3078d70bbb31bff7dfae3094d27886231f
Qesait <kuzva2003@gmail.com>
2022-11-26 12:39:53 +0300

    цвета

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 from PySide6.QtCore import *
 from PySide6.QtGui import *
@@ -7,13 +8,24 @@ from PySide6.QtWidgets import *
 from schedule import Lesson, Schedule
 
 
+DARK_GREEN = '#61892F'
+LIGHT_GREEN = '#86C232'
+BLACK = '#222629'
+DARK_GREY = '#474B4F'
+LIGHT_GREY = '#6B6E70'
+
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.setMinimumSize(QSize(480, 320))
         self.setBaseSize(QSize(960, 640))
-        self.setCentralWidget(Schedule())
+        with open('schedule.json') as f:
+            schedule = json.load(f)
+        schedule = schedule['Monday']
+        self.setCentralWidget(Schedule(schedule))
+        self.setStyleSheet(f'MainWindow {{background-color: {BLACK};}}')
 
 
 if __name__ == "__main__":
--- schedule.py
+++ schedule.py
@@ -5,32 +5,57 @@ from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
 
-class Color(QWidget):
+DARK_GREEN = '#61892F'
+LIGHT_GREEN = '#86C232'
+BLACK = '#222629'
+DARK_GREY = '#474B4F'
+LIGHT_GREY = '#6B6E70'
+
+
+class Color(QAbstractButton):
 
     def __init__(self, color):
         super(Color, self).__init__()
         self.setAutoFillBackground(True)
 
         palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        palette.setColor(QPalette.ColorRole.Window, DARK_GREY)
         self.setPalette(palette)
 
 
 class Lesson(QWidget):
-    def __init__(self):
+    def __init__(self, data):
         super().__init__()
+        self.time_layout = QHBoxLayout()
+        self.time_layout.addWidget(QLabel(data['time']))
+        self.time_layout.addStretch(1)
+
         self.v_layout = QVBoxLayout()
-        self.v_layout.addWidget(Color('blue'))
-        self.v_layout.addWidget(Color('green'))
+        self.v_layout.addWidget(QLabel(data['name']))
+        self.v_layout.addLayout(self.time_layout)
+        self.v_layout.addStretch(1)
 
         self.h_layout = QHBoxLayout(self)
         self.h_layout.addLayout(self.v_layout)
-        self.h_layout.addWidget(Color('red'))
+        self.h_layout.addWidget(QLabel(data['text']), 10)
+
+        # self.setAutoFillBackground(True)
+        # palette = self.palette()
+        # palette.setColor(QPalette.ColorRole.Window, DARK_GREY)
+        # self.setPalette(palette)
 
 
 class Schedule(QWidget):
-    def __init__(self):
+    def __init__(self, schedule):
         super().__init__()
         self.lessons = QVBoxLayout(self)
-        for i in range(5):
-            self.lessons.addWidget(Lesson())
+        for lesson in schedule:
+            self.lessons.addWidget(Lesson(lesson))
+
+        self.setStyleSheet(
+            f'''QLabel {{
+            background-color: {DARK_GREY}; 
+            color: white;
+            padding: 6px;
+            border-radius: 10px;
+            }}''')
